8a1131fcb73f63397f7b3b02367bc2f44abb7a0c
Jimin <77067383+wl2258@users.noreply.github.com>
2022-05-28 22:25:07 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./styles.css";
+
+const NetflixHeader = () => {
+  return <h3>넷플릭스 추천작</h3>;
+};
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+      <NetflixHeader />
+    </div>
+  );
+}
